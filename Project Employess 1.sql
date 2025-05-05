427ece6845e1bfc6ec005b1cc918ffa2f829f153
Md Al Azizu <alazizu6798@gmail.com>
2025-05-05 14:54:19 +0530

    Add files via upload

---
+++ Project Employess 1.sql
@@ -0,0 +1,37 @@
+
+Table: Project
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| project_id  | int     |
+| employee_id | int     |
++-------------+---------+
+(project_id, employee_id) is the primary key of this table.
+employee_id is a foreign key to Employee table.
+Each row of this table indicates that the employee with employee_id is working on the project with project_id.
+ 
+
+Table: Employee
+
++------------------+---------+
+| Column Name      | Type    |
++------------------+---------+
+| employee_id      | int     |
+| name             | varchar |
+| experience_years | int     |
++------------------+---------+
+employee_id is the primary key of this table. It's guaranteed that experience_years is not NULL.
+Each row of this table contains information about one employee.
+ 
+
+Write an SQL query that reports the average experience years of all the employees for each project, rounded to 2 digits.
+
+Return the result table in any order.
+
+# MySQL query statement below
+
+select project_id, round(sum(experience_years)/count(experience_years),2) as average_years
+from employee E right join project PE
+on E.employee_id = PE.employee_id
+group by project_id
